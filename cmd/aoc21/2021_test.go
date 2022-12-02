53b178b79d552f86a50d831baa46ed60691d0704
Ben Lechlitner <otherland@gmail.com>
2022-12-02 00:29:02 -0500

    2021 day 2

--- cmd/aoc21/2021_test.go
+++ cmd/aoc21/2021_test.go
@@ -18,7 +18,8 @@ func Test_2021(t *testing.T) {
 	}{
 		{"2021-1a Example", aoc21.Day1part1, []string{"199", "200", "208", "210", "200", "207", "240", "269", "260", "263"}, "7"},
 		{"2021-1b Example", aoc21.Day1part2, []string{"199", "200", "208", "210", "200", "207", "240", "269", "260", "263"}, "5"},
-		// {"2021-1a", Day2part1, []string{"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"}, 150},
+		{"2021-2a Example", aoc21.D2P1, []string{"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"}, "150"},
+		{"2021-2a Example", aoc21.D2P2, []string{"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"}, "900"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
