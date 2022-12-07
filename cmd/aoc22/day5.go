7e4adbbb61b4b16f20407d6871d1f874f8e3b122
Ben Lechlitner <otherland@gmail.com>
2022-12-06 19:53:15 -0500

    2022 day 5 full solution

--- cmd/aoc22/day5.go
+++ cmd/aoc22/day5.go
@@ -11,91 +11,143 @@ func init() { //nolint:gochecknoinits // init needed to register command
 	common.NewDayCommand(2022, 5, D5P1, D5P2, Get2022Command())
 }
 
+// Action is the struct for an action.
+type Action struct {
+	qty int
+	src int
+	tgt int
+}
+
+// Day5 is the struct for day 5.
+type Day5 struct {
+	Stacks  [][]string
+	Actions []Action
+}
+
 // D5P1 returns the solution for 2022 day 5 part 1
-// answer:
+// answer: SHMSDGZVC
 func D5P1(data []string) string {
-	// get number of stacks
-	stackCount := getNumberOfStacks(data[0])
-	fmt.Printf("%d stacks found\n", stackCount)
-
-	stacks := make([][]string, stackCount)
-
-	for i := 0; i < stackCount; i++ {
-		stacks[i] = parseStack(data, i)
+	day := Day5{
+		Stacks:  [][]string{},
+		Actions: []Action{},
 	}
 
-	for i, v := range stacks {
-		fmt.Printf("stack[%d]: %v\n", i+1, v)
+	// read crates into appropriate stacks
+	day.populateStacks(data)
+
+	// debugPrintStacks(day)
+
+	// parse actions and store in day struct
+	err := day.populateActions(data)
+	if err != nil {
+		return fmt.Sprintf("failed to parse stacks: %v", err)
 	}
 
-	var (
-		qty int
-		src int
-		tgt int
-	)
+	// fmt.Printf("actions: %+v\n", day.Actions)
 
-	for i := getNumberOfStacks(data[0]) + 2; i < len(data); i++ {
-		fmt.Printf("line %d: %s\n", i, data[i])
+	// loop through actions and apply to stacks
+	err = day.processActionsSingleMove()
+	if err != nil {
+		return fmt.Sprintf("failed to parse actions: %v", err)
+	}
 
-		_, err := fmt.Sscanf(data[i], "move %d from %d to %d", &qty, &src, &tgt)
+	return day.topCrates()
+}
+
+func (day *Day5) populateActions(data []string) error {
+	for i := GetMovementSectionLine(data); i < len(data); i++ {
+		a := Action{}
+
+		// fmt.Printf("line %d: %s\n", i, data[i])
+
+		_, err := fmt.Sscanf(data[i], "move %d from %d to %d", &a.qty, &a.src, &a.tgt)
 		if err != nil {
-			return fmt.Sprintf("failed to parse line \"%s\": %v", data[i], err)
+			return fmt.Errorf("failed to parse line \"%s\": %w", data[i], err)
 		}
 
 		// normalize stack numbers to indexes
-		// move each crate separately
-		for i := 0; i < qty; i++ {
+		a.src--
+		a.tgt--
 
+		day.Actions = append(day.Actions, a)
+	}
+
+	return nil
+}
+
+func (day *Day5) processActionsSingleMove() error {
+	for _, a := range day.Actions {
+		// move each crate separately
+		for i := 0; i < a.qty; i++ {
 			// pop
-			n := len(stacks[src-1]) - 1
-			c := stacks[src-1][n]
-			stacks[src-1] = stacks[src-1][:n]
+			n := len(day.Stacks[a.src]) - 1
+			if n < 0 {
+				return fmt.Errorf("stack %d is empty", a.src)
+			}
+
+			c := day.Stacks[a.src][n]
+			day.Stacks[a.src] = day.Stacks[a.src][:n]
+
+			// fmt.Printf("moved %s; %d -> %d\n", c, a.src, a.tgt)
 
 			// push
-			stacks[tgt-1] = append(stacks[tgt-1], c)
-
-			fmt.Printf("moved %s; %d -> %d\n", c, src-1, tgt-1)
+			day.Stacks[a.tgt] = append(day.Stacks[a.tgt], c)
 		}
 	}
 
-	return printStacks(stacks)
+	return nil
 }
 
-// D5P2 returns the solution for 2022 day 5 part 2
-// answer:
-func D5P2(data []string) string {
-	return "not implemented"
+func (day *Day5) populateStacks(data []string) {
+	stackCount := GetNumberOfStacks(data[0])
+	// fmt.Printf("%d stacks found\n", stackCount)
+
+	day.Stacks = make([][]string, stackCount)
+
+	for i := 0; i < stackCount; i++ {
+		day.Stacks[i] = ParseStack(data, i)
+	}
 }
 
-func getNumberOfStacks(data string) int {
+// func debugPrintStacks(day Day5) {
+// 	for i, v := range day.Stacks {
+// 		fmt.Printf("stack[%d]: %v\n", i+1, v)
+// 	}
+// }
+
+// GetNumberOfStacks returns the size of the stack section. This does not include stack numbers or the blank line.
+func GetNumberOfStacks(data string) int {
 	return (len(data) + 1) / 4
 }
 
-func getStackHeight(data []string) int {
-	h := 0
-
-	for i := 0; i < len(data); i++ {
-		if data[i][:2] == " 1" {
-			h = i + 1
-			break
+// GetMovementSectionLine returns the line number where the movement section starts.
+func GetMovementSectionLine(data []string) (h int) {
+	for h = 0; h < len(data); h++ {
+		if data[h] != "" && strings.HasPrefix(data[h], "move") {
+			return
 		}
 	}
 
-	// fmt.Printf("stack height is: %d\n", h)
+	fmt.Println("failed to find movement section")
 
-	return h
+	return -1
 }
 
-func parseStack(data []string, stackNumber int) []string {
-	height := getStackHeight(data)
-	stack := make([]string, 0)
+// ParseStack returns a slice of strings representing the stack at the given index.
+func ParseStack(data []string, stackIndex int) []string {
+	if stackIndex < 0 || stackIndex >= GetNumberOfStacks(data[0]) {
+		fmt.Printf("invalid stack index: %d\n", stackIndex)
+		return nil
+	}
+
+	var stack []string
 
 	const w int = 4
 
 	crate := ""
 
-	for i := 0; i < height-1; i++ {
-		crate = data[i][stackNumber*w+1 : stackNumber*w+2]
+	for i := 0; !strings.HasPrefix(data[i], " 1"); i++ {
+		crate = string(data[i][(stackIndex*w)+1])
 		// fmt.Printf("[%d][%d]: '%s'", stackNumber, i, crate)
 
 		// don't add blank areas to stack
@@ -108,12 +160,63 @@ func parseStack(data []string, stackNumber int) []string {
 	return stack
 }
 
-func printStacks(stacks [][]string) string {
+func (day *Day5) topCrates() string {
 	var sb strings.Builder
 
-	for _, s := range stacks {
+	for _, s := range day.Stacks {
 		sb.WriteString(s[len(s)-1])
 	}
 
 	return sb.String()
+}
+
+// D5P2 returns the solution for 2022 day 5 part 2
+// answer: VRZGHDFBQ
+func D5P2(data []string) string {
+	day := Day5{
+		Stacks:  [][]string{},
+		Actions: []Action{},
+	}
+
+	// read crates into appropriate stacks
+	day.populateStacks(data)
+
+	// debugPrintStacks(day)
+
+	// parse actions and store in day struct
+	err := day.populateActions(data)
+	if err != nil {
+		return fmt.Sprintf("failed to parse stacks: %v", err)
+	}
+
+	// fmt.Printf("actions: %+v\n", day.Actions)
+
+	// loop through actions and apply to stacks
+	err = day.processActionsBulkMove()
+	if err != nil {
+		return fmt.Sprintf("failed to parse actions: %v", err)
+	}
+
+	return day.topCrates()
+}
+
+func (day *Day5) processActionsBulkMove() error {
+	for _, a := range day.Actions {
+		// pop
+		n := len(day.Stacks[a.src])
+		if n < 0 {
+			return fmt.Errorf("stack %d is empty", a.src)
+		}
+
+		b := n - a.qty
+		c := day.Stacks[a.src][b:]
+		day.Stacks[a.src] = day.Stacks[a.src][:b]
+
+		// fmt.Printf("moved %s; %d -> %d\n", c, a.src, a.tgt)
+
+		// push
+		day.Stacks[a.tgt] = append(day.Stacks[a.tgt], c...)
+	}
+
+	return nil
 }
--- cmd/aoc22/day5_test.go
+++ cmd/aoc22/day5_test.go
@@ -16,10 +16,45 @@ func Test_Day5(t *testing.T) {
 		input []string
 		want  string
 	}{
-		{"Part 1 Example", aoc22.D5P1, []string{
+		{"No Change - Single", aoc22.D5P1, []string{
 			"    [D]    ",
 			"[N] [C]    ",
 			"[Z] [M] [P]",
+			" 1   2   3 ",
+			"",
+			"move 1 from 2 to 1",
+			"move 1 from 1 to 2",
+		}, "NDP"},
+		{"No Change - Bulk", aoc22.D5P2, []string{
+			"    [D]    ",
+			"[N] [C]    ",
+			"[Z] [M] [P]",
+			" 1   2   3 ",
+			"",
+			"move 2 from 2 to 1",
+			"move 2 from 1 to 2",
+		}, "NDP"},
+		{"One Change - Single", aoc22.D5P1, []string{
+			"    [D]    ",
+			"[N] [C]    ",
+			"[Z] [M] [P]",
+			" 1   2   3 ",
+			"",
+			"move 1 from 2 to 1",
+		}, "DCP"},
+		{"One Change - Bulk", aoc22.D5P2, []string{
+			"    [D]    ",
+			"[N] [C]    ",
+			"[Z] [M] [P]",
+			" 1   2   3 ",
+			"",
+			"move 2 from 2 to 1",
+		}, "DMP"},
+		{"Part 1 Example", aoc22.D5P1, []string{
+			//                                    Z |     Z |     Z
+			"    [D]    ", //   D   | D     |     N |     N |     N
+			"[N] [C]    ", // N C   | N C   |   C D | M   D |     D
+			"[Z] [M] [P]", // Z M P | Z M P |   M P | C   P | C M P
 			" 1   2   3 ",
 			"",
 			"move 1 from 2 to 1",
@@ -27,12 +62,111 @@ func Test_Day5(t *testing.T) {
 			"move 2 from 2 to 1",
 			"move 1 from 1 to 2",
 		}, "CMZ"},
+		{"Part 2 Example", aoc22.D5P2, []string{
+			//                                    D |     D |     D
+			"    [D]    ", //   D   | D     |     N |     N |     N
+			"[N] [C]    ", // N C   | N C   |   C Z | C   Z |     Z
+			"[Z] [M] [P]", // Z M P | Z M P |   M P | M   P | M C P
+			" 1   2   3 ",
+			"",
+			"move 1 from 2 to 1",
+			"move 3 from 1 to 3",
+			"move 2 from 2 to 1",
+			"move 1 from 1 to 2",
+		}, "MCD"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.code(tt.input); got != tt.want {
-				assert.Equal(t, tt.want, got)
-			}
+			got := tt.code(tt.input)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_GetMovementSectionLine(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []string
+		want  int
+	}{
+		{"Part 1 Example", []string{
+			"    [D]    ",
+			"[N] [C]    ",
+			"[Z] [M] [P]",
+			" 1   2   3 ",
+			"",
+			"move 1 from 2 to 1",
+			"move 1 from 1 to 2",
+		}, 5},
+		{"All One", []string{
+			"[Z] [M] [P]",
+			" 1   2   3 ",
+			"",
+			"move 1 from 2 to 1",
+			"move 3 from 1 to 3",
+			"move 2 from 2 to 1",
+			"move 1 from 1 to 2",
+		}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := aoc22.GetMovementSectionLine(tt.input)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_GetNumberOfStacks(t *testing.T) {
+	tests := []struct {
+		name  string
+		input string
+		want  int
+	}{
+		{"Example", "    [D]    ", 3},
+		{"Day Input", "[G]                 [D] [R]        ", 9},
+		{"One", "[G]", 1},
+		{"Two", "[G] [D]", 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := aoc22.GetNumberOfStacks(tt.input)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_ParseStack(t *testing.T) {
+	type args struct {
+		input  []string
+		stacks int
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			"Part 1 Example",
+			args{
+				[]string{
+					"    [D]    ",
+					"[N] [C]    ",
+					"[Z] [M] [P]",
+					" 1   2   3 ",
+					"",
+					"move 1 from 2 to 1",
+					"move 1 from 1 to 2",
+				},
+				1,
+			},
+			[]string{"M", "C", "D"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := aoc22.ParseStack(tt.args.input, tt.args.stacks)
+			assert.Equal(t, tt.want, got)
 		})
 	}
 }
