d75a70a9d76bfa89d3684d46d8807d49e9a079b2
Ben Lechlitner <otherland@gmail.com>
2023-01-03 10:55:45 -0500

    stub day13

---
+++ exercises/aoc22_13/day13.go
@@ -0,0 +1,17 @@
+// Package aoc22_13 contains the solution for day 13 of Advent of Code 2022.
+package aoc22_13 //nolint:revive,stylecheck // I don't care about the package name
+
+// D13P1 returns the solution for 2022 day 13 part 1.
+//
+// https://adventofcode.com/2022/day/13
+//
+// answer:
+func D13P1(data []string) string {
+	return ""
+}
+
+// D13P2 returns the solution for 2022 day 13 part 2.
+// answer:
+func D13P2(data []string) string {
+	return ""
+}
