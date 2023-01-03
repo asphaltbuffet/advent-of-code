fe0e38b1ca0adeeac194af446a5243b9dcb5185f
Ben Lechlitner <otherland@gmail.com>
2023-01-03 11:26:43 -0500

    stub day 22

---
+++ cmd/aoc22/day22.go
@@ -0,0 +1,10 @@
+package aoc22
+
+import (
+	"github.com/asphaltbuffet/advent-of-code/exercises/aoc22_22"
+	"github.com/asphaltbuffet/advent-of-code/internal/common"
+)
+
+func init() { //nolint:gochecknoinits // init needed to register command
+	common.NewDayCommand(2022, 22, aoc22_22.D22P1, aoc22_22.D22P2, Get2022Command())
+}
---
+++ exercises/aoc22_22/day22.go
@@ -0,0 +1,17 @@
+// Package aoc22_22 contains the solution for day 22 of Advent of Code 2022.
+package aoc22_22 //nolint:revive,stylecheck // I don't care about the package name
+
+// D22P1 returns the solution for 2022 day 22 part 1.
+//
+// https://adventofcode.com/2022/day/22
+//
+// answer:
+func D22P1(data []string) string {
+	return ""
+}
+
+// D22P2 returns the solution for 2022 day 22 part 2.
+// answer:
+func D22P2(data []string) string {
+	return ""
+}
