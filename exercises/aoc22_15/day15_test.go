0cc39bd132ab979a9d9e9c493d2137111f085fb1
Ben Lechlitner <otherland@gmail.com>
2023-02-09 22:39:25 -0500

    add part1 example

--- exercises/aoc22_15/day15_test.go
+++ exercises/aoc22_15/day15_test.go
@@ -14,10 +14,21 @@ func Test_Day15Part1(t *testing.T) {
 		input []string
 		want  string
 	}{
-		// {"Part 1 Example", []string{
-		// 	"input",
-		// 	"strings",
-		// }, "answer"},
+		{"Part 1 Example", []string{
+			"Sensor at x=9, y=16: closest beacon is at x=10, y=16",
+			"Sensor at x=13, y=2: closest beacon is at x=15, y=3",
+			"Sensor at x=12, y=14: closest beacon is at x=10, y=16",
+			"Sensor at x=10, y=20: closest beacon is at x=10, y=16",
+			"Sensor at x=14, y=17: closest beacon is at x=10, y=16",
+			"Sensor at x=8, y=7: closest beacon is at x=2, y=10",
+			"Sensor at x=2, y=0: closest beacon is at x=2, y=10",
+			"Sensor at x=0, y=11: closest beacon is at x=2, y=10",
+			"Sensor at x=20, y=14: closest beacon is at x=25, y=17",
+			"Sensor at x=17, y=20: closest beacon is at x=21, y=22",
+			"Sensor at x=16, y=7: closest beacon is at x=15, y=3",
+			"Sensor at x=14, y=3: closest beacon is at x=15, y=3",
+			"Sensor at x=20, y=1: closest beacon is at x=15, y=3",
+		}, "26"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -34,7 +45,8 @@ func Test_Day15Part2(t *testing.T) {
 		want  string
 	}{
 		// {"Part 2 Example", []string{
-		// 	"input",
+		// 	Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+		// "input"
 		// 	"strings",
 		// }, "answer"},
 	}
