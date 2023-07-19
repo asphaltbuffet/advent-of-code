edd32909d465a2121626c4f97e49f719113b014f
Ben Lechlitner <otherland@gmail.com>
2023-07-19 00:06:53 -0400

    python solution for aoc22-4

--- exercises/2022/04-campCleanup/README.md
+++ exercises/2022/04-campCleanup/README.md
@@ -2,14 +2,28 @@
 
 ## Go
 
-```console
+```text
 2022-4 Camp Cleanup (Golang)
 
 Running...
 
-Test 1.0: pass in 0.0000 seconds
-Test 1.1: pass in 0.0000 seconds
-Test 2.0: pass in 0.0000 seconds
-Part 1: 532 in 0.0011 seconds
-Part 2: 854 in 0.0011 seconds
+Test 1.0: pass in 20.4 µs
+Test 1.1: pass in 11.9 µs
+Test 2.0: pass in 12.3 µs
+Part 1: 532 in 1.075 ms
+Part 2: 854 in 1.0727 ms
+```
+
+## Python
+
+```text
+2022-4 Camp Cleanup (Python)
+
+Running...
+
+Test 1.0: pass in 59.366226 µs
+Test 1.1: pass in 50.544739 µs
+Test 2.0: pass in 31.232834 µs
+Part 1: 532 in 1.852751 ms
+Part 2: 854 in 1.85585 ms
 ```
--- exercises/2022/04-campCleanup/py/__init__.py
+++ exercises/2022/04-campCleanup/py/__init__.py
@@ -1,12 +1,52 @@
 from typing import *
 from aocpy import BaseExercise
 
+import re
+
+
 # Exercise for Advent of Code 2022 day 4
 class Exercise(BaseExercise):
     @staticmethod
     def one(instr: str) -> int:
-        raise NotImplementedError
+        count = 0
+        for line in instr.split("\n"):
+            elf_one, elf_two = parse_pair(line)
+            if elf_one and elf_two and is_fully_overlapping(elf_one, elf_two):
+                count += 1
+        return count
 
     @staticmethod
     def two(instr: str) -> int:
-        raise NotImplementedError
+        count = 0
+        for line in instr.split("\n"):
+            elf_one, elf_two = parse_pair(line)
+            if elf_one and elf_two and is_any_overlapping(elf_one, elf_two):
+                count += 1
+        return count
+
+
+class Elf:
+    def __init__(self, low, high) -> None:
+        self.low = low
+        self.high = high
+
+
+def is_any_overlapping(a, b) -> bool:
+    return (a.low <= b.low and a.high >= b.low) or (b.low <= a.low and b.high >= a.low)
+
+
+def is_fully_overlapping(a, b) -> bool:
+    return (a.low <= b.low and a.high >= b.high) or (
+        b.low <= a.low and b.high >= a.high
+    )
+
+
+def parse_pair(line) -> Tuple[Elf, Elf]:
+    try:
+        a_low, a_high, b_low, b_high = map(int, re.findall(r"\d+", line))
+        a = Elf(a_low, a_high)
+        b = Elf(b_low, b_high)
+    except ValueError:
+        print(f"error parsing line: {line}")
+        a = b = None
+    return a, b
