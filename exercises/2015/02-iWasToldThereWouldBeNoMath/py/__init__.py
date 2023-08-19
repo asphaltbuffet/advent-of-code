0587a2554b52f786e33263ef77ae51cf320ec41e
Ben Lechlitner <otherland@gmail.com>
2023-08-18 23:10:03 -0400

    aoc15-2 part 2 py

--- exercises/2015/02-iWasToldThereWouldBeNoMath/py/__init__.py
+++ exercises/2015/02-iWasToldThereWouldBeNoMath/py/__init__.py
@@ -3,11 +3,20 @@ from aocpy import BaseExercise
 
 
 def parse(instr: str) -> List[Tuple[int, int, int]]:
+    """Parses the input string into a list of int tuples."""
     dims = []
     for line in instr.splitlines():
         dims.append(tuple(int(i) for i in line.split("x")))
 
     return dims
+
+
+def prod(t: Iterable) -> int:
+    """Returns the product of all elements in t."""
+    p = 1
+    for i in t:
+        p *= i
+    return p
 
 
 # Exercise for Advent of Code 2015 day 2.
@@ -30,4 +39,13 @@ class Exercise(BaseExercise):
 
     @staticmethod
     def two(instr: str) -> int:
-        raise NotImplementedError
+        packages = parse(instr)
+
+        total = 0
+        for p in packages:
+            perimeter = 2 * (sum(p) - max(p))
+            area = prod(p)
+
+            total += perimeter + area
+
+        return total
