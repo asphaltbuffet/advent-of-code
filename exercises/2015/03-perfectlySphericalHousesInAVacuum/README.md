a44f8a00d8b0b46c0b402e8cdcf17bbeaa4ea1c0
Ben Lechlitner <otherland@gmail.com>
2023-08-19 00:40:22 -0400

    aoc15-3 full py

---
+++ exercises/2015/03-perfectlySphericalHousesInAVacuum/README.md
@@ -0,0 +1,29 @@
+# [Day 3: Perfectly Spherical Houses In A Vacuum](https://adventofcode.com/2015/day/3)
+
+<!-- These are helper text to make formatting the yearly readme consistent and easier...
+
+[Day 3: Perfectly Spherical Houses In A Vacuum][rm3]
+[Go][g3]
+[Python][p3]
+
+[rm3]: 03-perfectlySphericalHousesInAVacuum/README.md
+[g3]: 03-perfectlySphericalHousesInAVacuum/go
+[p3]: 03-perfectlySphericalHousesInAVacuum/py
+
+-->
+
+## Go
+
+```text
+    < section intentionally left blank >
+```
+
+## Python
+
+```text
+    < section intentionally left blank >
+```
+
+## 2015 Run Times
+
+![2015 exercise run-time graphs](../run-times.png)
---
+++ exercises/2015/03-perfectlySphericalHousesInAVacuum/py/__init__.py
@@ -0,0 +1,47 @@
+from typing import *
+from aocpy import BaseExercise
+
+
+def move(x: int, y: int, c: str) -> Tuple[int, int]:
+    if c == "^":
+        y -= 1
+    elif c == "v":
+        y += 1
+    elif c == ">":
+        x += 1
+    elif c == "<":
+        x -= 1
+
+    return x, y
+
+
+# Exercise for Advent of Code 2015 day 3.
+class Exercise(BaseExercise):
+    @staticmethod
+    def one(instr: str) -> int:
+        x, y = 0, 0
+        visited = set()
+        visited.add((0, 0))
+
+        for i, c in enumerate(instr):
+            x, y = move(x, y, c)
+            visited.add((x, y))
+
+        return len(visited)
+
+    @staticmethod
+    def two(instr: str) -> int:
+        x, y = 0, 0
+        a, b = 0, 0
+        visited = set()
+        visited.add((0, 0))
+
+        for i, c in enumerate(instr):
+            if i % 2 == 0:
+                x, y = move(x, y, c)
+                visited.add((x, y))
+            else:
+                a, b = move(a, b, c)
+                visited.add((a, b))
+
+        return len(visited)
