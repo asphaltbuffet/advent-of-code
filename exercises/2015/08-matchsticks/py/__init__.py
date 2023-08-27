aa166ca0548444ef236e52f4af0deb4c77d568a5
Ben Lechlitner <otherland@gmail.com>
2023-08-26 22:46:56 -0400

    aoc15-8: part 2 py

--- exercises/2015/08-matchsticks/py/__init__.py
+++ exercises/2015/08-matchsticks/py/__init__.py
@@ -4,6 +4,23 @@ from aocpy import BaseExercise
 import re
 
 
+def strip(s: str) -> str:
+    s = re.sub(r'\\"', r'"', s)
+    s = re.sub(r"\\\\", r"\\", s)
+    s = re.sub(r"\\x[0-9a-f]{2}", r"x", s)
+    # print(f"{count} += {orig} - {len(line) - 2}")
+
+    return s
+
+
+def escape(s: str) -> str:
+    s = re.sub(r"\\", r"\\\\", s)
+    s = re.sub(r'"', r'\\"', s)
+    # s = re.sub(r"\\x[0-9a-f]{2}", r"x", s)
+
+    return s
+
+
 # Exercise for Advent of Code 2015 day 8.
 class Exercise(BaseExercise):
     @staticmethod
@@ -13,15 +30,21 @@ class Exercise(BaseExercise):
         count = 0
         for line in lines:
             orig = len(line)
-            line = re.sub(r'\\"', r'"', line)
-            line = re.sub(r"\\\\", r"\\", line)
-            line = re.sub(r"\\x[0-9a-f]{2}", r"x", line)
-            # print(f"{count} += {orig} - {len(line) - 2}")
-
-            count += orig - (len(line) - 2)
+            stripped = strip(line)
+            count += orig - (len(stripped) - 2)
 
         return count
 
     @staticmethod
     def two(instr: str) -> int:
-        raise NotImplementedError("part 2 not implemented")
+        lines = instr.splitlines()
+
+        count = 0
+        for line in lines:
+            orig = len(line)
+            esc = f'"{escape(line)}"'
+            # print(f"{line} -> {esc}")
+            # print(f"{count} += {len(esc)} - {orig}")
+            count += len(esc) - orig
+
+        return count
