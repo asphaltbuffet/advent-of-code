2e0049c78ccaadc38c58f3d9d2931073008d28d4
Ben Lechlitner <otherland@gmail.com>
2023-10-15 00:10:18 -0400

    fix linting issues

--- pkg/utilities/math.go
+++ pkg/utilities/math.go
@@ -1,6 +1,6 @@
 package utilities
 
-// MeanIntSlice returns the mean of a slice of floats.
+// MeanFloatSlice returns the mean of a slice of floats.
 func MeanFloatSlice(arr []float64) float64 {
 	var sum float64
 	for _, v := range arr {
@@ -10,7 +10,7 @@ func MeanFloatSlice(arr []float64) float64 {
 	return sum / float64(len(arr))
 }
 
-// MinIntSlice returns the minimum value of a slice of floats.
+// MinFloatSlice returns the minimum value of a slice of floats.
 func MinFloatSlice(arr []float64) float64 {
 	min := arr[0]
 	for _, v := range arr {
@@ -22,7 +22,7 @@ func MinFloatSlice(arr []float64) float64 {
 	return min
 }
 
-// MaxIntSlice returns the maximum value of a slice of floats.
+// MaxFloatSlice returns the maximum value of a slice of floats.
 func MaxFloatSlice(arr []float64) float64 {
 	max := arr[0]
 	for _, v := range arr {
