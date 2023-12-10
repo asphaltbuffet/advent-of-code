2f5c44627ae346bf9053511abca467c0a297a13d
Ben Lechlitner <otherland@gmail.com>
2023-12-10 14:14:22 -0500

    export answers to toml format

---
+++ exercises/2023/answers.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/zsh
+
+fd info -X \
+    jq -r '.day as $day | .data.answers | to_entries | .[] | "\($day)." + (if .key == "a" then "1" else "2" end) + "=\"" + .value + "\""'| \
+    sort
