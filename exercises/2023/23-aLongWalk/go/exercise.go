8f91fe4c91909eab323c140240e8fc0f4f245288
Ben Lechlitner <otherland@gmail.com>
2024-01-29 16:02:53 -0500

    solution: day 23

--- exercises/2023/23-aLongWalk/go/exercise.go
+++ exercises/2023/23-aLongWalk/go/exercise.go
@@ -1,8 +1,6 @@
 package exercises
 
 import (
-	"fmt"
-
 	"github.com/asphaltbuffet/advent-of-code/internal/common"
 )
 
@@ -12,11 +10,25 @@ type Exercise struct {
 }
 
 // One returns the answer to the first part of the exercise.
-func (e Exercise) One(instr string) (any, error) {
-	return nil, fmt.Errorf("part 1 not implemented")
+func (e Exercise) One(input string) (any, error) {
+	tm := parseInput(input)
+
+	visited := map[Point]int{tm.Start: 0}
+	tm.walk(tm.Start, Point{0, 1}, visited)
+
+	return visited[tm.End], nil
 }
 
 // Two returns the answer to the second part of the exercise.
-func (e Exercise) Two(instr string) (any, error) {
-	return nil, fmt.Errorf("part 2 not implemented")
+func (e Exercise) Two(input string) (any, error) {
+	tm := parseInput(input)
+
+	tm.walk(tm.Start, Point{0, 1}, map[Point]int{tm.Start: 0})
+	tm.getJunctions()
+
+	paths := tm.getPaths()
+	visited := make([]bool, len(tm.Junctions))
+	visited[paths[tm.Start][0].index] = true
+
+	return tm.getLongestPath(paths, tm.Start, tm.End, 0, visited), nil
 }
