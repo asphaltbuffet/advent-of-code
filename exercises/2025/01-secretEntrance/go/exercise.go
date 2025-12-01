a517d740b3f17fda615df904c8023b07a475516e
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2025-12-01 09:24:16 -0500

    day 1 part 1

--- exercises/2025/01-secretEntrance/go/exercise.go
+++ exercises/2025/01-secretEntrance/go/exercise.go
@@ -2,6 +2,8 @@ package exercises
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/asphaltbuffet/advent-of-code/internal/common"
 )
@@ -13,7 +15,27 @@ type Exercise struct {
 
 // One returns the answer to the first part of the exercise.
 func (e Exercise) One(instr string) (any, error) {
-	return nil, fmt.Errorf("part 1 not implemented")
+	var zero int
+	pos := 50
+
+	for _, l := range strings.Split(instr, "\n") {
+		direction := l[0]
+		val, _ := strconv.Atoi(l[1:])
+
+		if direction == 'L' {
+			pos = pos + 100 - (val % 100)
+		} else {
+			pos = pos + val
+		}
+
+		pos %= 100
+
+		if pos == 0 {
+			zero++
+		}
+	}
+
+	return fmt.Sprintf("%d", zero), nil
 }
 
 // Two returns the answer to the second part of the exercise.
