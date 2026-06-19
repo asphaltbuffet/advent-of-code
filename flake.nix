c07ebfa5ddb83b2312df2a8d2bf4ef3169b17547
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2026-06-18 20:36:03 -0400

    tooling updates

--- flake.nix
+++ flake.nix
@@ -5,12 +5,17 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
     flake-utils.url = "github:numtide/flake-utils";
     nur.url = "github:nix-community/NUR";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     nixpkgs,
     flake-utils,
     nur,
+    fenix,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (
@@ -19,20 +24,34 @@
           inherit system;
           overlays = [nur.overlays.default];
         };
+        rustToolchain = fenix.packages.${system}.stable.withComponents [
+          "cargo"
+          "clippy"
+          "rust-src"
+          "rustc"
+          "rustfmt"
+        ];
       in {
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
-            go
             jujutsu
             jjui
             ripgrep
             fd
             sd
+            parallel
             gopls
             nixd
             uv
             nodejs
             gh
+
+            go
+            python3
+            gfortran
+
+            rustToolchain
+            fenix.packages.${system}.rust-analyzer
           ];
 
           CGO_ENABLED = "0";
