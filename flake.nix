75108dc124b64a1b8802b8d2a510c9b5c4072914
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2026-07-07 23:44:20 -0400

    chore: bump elf to v0.5.0

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     elf = {
-      url = "github:asphaltbuffet/elf/v0.4.4";
+      url = "github:asphaltbuffet/elf/v0.5.0";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
