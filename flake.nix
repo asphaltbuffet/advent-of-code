4b0f86f493a8770da265b5274d775f5e5dfc1335
Ben Lechlitner <30903912+asphaltbuffet@users.noreply.github.com>
2026-07-13 16:03:40 -0400

    update elf

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     elf = {
-      url = "github:asphaltbuffet/elf/v0.5.0";
+      url = "github:asphaltbuffet/elf/v0.6.1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
